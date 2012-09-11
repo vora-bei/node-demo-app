96d94155cc13061643c9fec339b517308e39d9c3
Vorobyov <voronsupermen@gmail.com>
2012-09-11 23:35:02 +0600

    копипаст из примера

--- app.js
+++ app.js
@@ -48,9 +48,81 @@ function loadUser(req, res, next) {
 }
 
 
+app.get('/404', function(req, res) {
+    throw new NotFound;
+});
+
+app.get('/500', function(req, res) {
+    throw new Error('An expected error');
+});
+
+app.get('/bad', function(req, res) {
+    unknownMethod();
+});
+
+function authenticateFromLoginToken(req, res, next) {
+    var cookie = JSON.parse(req.cookies.logintoken);
+
+    LoginToken.findOne({ email: cookie.email,
+        series: cookie.series,
+        token: cookie.token }, (function(err, token) {
+        if (!token) {
+            res.redirect('/sessions/new');
+            return;
+        }
+
+        User.findOne({ email: token.email }, function(err, user) {
+            if (user) {
+                req.session.user_id = user.id;
+                req.currentUser = user;
+
+                token.token = token.randomToken();
+                token.save(function() {
+                    res.cookie('logintoken', token.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
+                    next();
+                });
+            } else {
+                res.redirect('/sessions/new');
+            }
+        });
+    }));
+}
+// Users
+app.get('/users/new', function(req, res) {
+    res.render('users/new.jade', {
+        locals: { user: new User() }
+    });
+});
+
+app.post('/users.:format?', function(req, res) {
+    var user = new User(req.body.user);
+
+    function userSaveFailed() {
+        req.flash('error', 'Account creation failed');
+        res.render('users/new.jade', {
+            locals: { user: user }
+        });
+    }
+user.save(function(err) {
+    if (err) return userSaveFailed();
+
+    req.flash('info', 'Your account has been created');
+    emails.sendWelcome(user);
+
+    switch (req.params.format) {
+        case 'json':
+            res.send(user.toObject());
+            break;
+
+        default:
+            req.session.user_id = user.id;
+            res.redirect('/documents');
+    }
+});
+
 // Сессии
 app.get('/sessions/new', function(req, res) {
-    res.render('sessions/new.jade', {
+    res.render('sessions/new', {
         locals: { user: new User() }
     });
 });
@@ -59,9 +131,32 @@ app.post('/sessions', function(req, res) {
     // Найти пользователя и выставить currentUser
 });
 
+app.post('/sessions', function(req, res) {
+    User.findOne({ email: req.body.user.email }, function(err, user) {
+        if (user && user.authenticate(req.body.user.password)) {
+            req.session.user_id = user.id;
+
+            // Remember me
+            if (req.body.remember_me) {
+                var loginToken = new LoginToken({ email: user.email });
+                loginToken.save(function() {
+                    res.cookie('logintoken', loginToken.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
+                    res.redirect('/documents');
+                });
+            } else {
+                res.redirect('/documents');
+            }
+        } else {
+            req.flash('error', 'Incorrect credentials');
+            res.redirect('/sessions/new');
+        }
+    });
+});
+
 app.del('/sessions', loadUser, function(req, res) {
-    // Удалить сессию
     if (req.session) {
+        LoginToken.remove({ email: req.currentUser.email }, function() {});
+        res.clearCookie('logintoken');
         req.session.destroy(function() {});
     }
     res.redirect('/sessions/new');
